5c8c2f706df84b1b0664327636b21817fd326ad5
小雨 <zsfys225011250@qq.com>
2024-01-27 14:59:37 +0000

    忘记win和lin不一样了新建个sh
    Signed-off-by: 小雨 <zsfys225011250@qq.com>

---
+++ Yunzai.sh
@@ -0,0 +1,10 @@
+git clone --depth=1 https://gitee.com/yoimiya-kokomi/Miao-Yunzai.git ./Miao-Yunzai/
+cd Miao-Yunzai
+git clone --depth=1 https://gitee.com/yoimiya-kokomi/miao-plugin.git ./plugins/miao-plugin/
+npm --registry=https://registry.npmmirror.com install pnpm -g
+pnpm config set registry https://registry.npmmirror.com
+pnpm install -P
+pnpm add sqlite3@5.0.0 -w
+pnpm add puppeteer@19.7.3 -w
+git clone --depth=1 https://gitee.com/xiaoye12123/ws-plugin.git ./plugins/ws-plugin/
+pnpm install --filter=ws-plugin
