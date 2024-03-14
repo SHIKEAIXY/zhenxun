a36ab43913e02f3f258bd2b1f17990a4b62aa4c0
小雨 <zsfys225011250@qq.com>
2024-03-14 14:23:47 +0000

    update Yunzai.sh.
    Signed-off-by: 小雨 <zsfys225011250@qq.com>

--- Yunzai.sh
+++ Yunzai.sh
@@ -1,11 +1,18 @@
+echo '正在下载喵崽'
 git clone --depth=1 https://gitee.com/yoimiya-kokomi/Miao-Yunzai.git ./Yunzai/Miao-Yunzai/
 cd Yunzai/Miao-Yunzai
+echo '正在下载喵喵插件'
 git clone --depth=1 https://gitee.com/yoimiya-kokomi/miao-plugin.git ./plugins/miao-plugin/
+echo '正在下载ws插件'
 git clone --depth=1 https://gitee.com/xiaoye12123/ws-plugin.git ./plugins/ws-plugin/
+echo '正在下载pnpm'
 npm --registry=https://registry.npmmirror.com install pnpm -g
-pnpm config set registry https://registry.npmmirror.com
+echo '正在安装依赖'
 pnpm i
+echo '正在降级puppeteer'
 pnpm add puppeteer@19.7.3 -w
 cd ..
+echo '正在下载redis数据库'
 git clone --depth 1 -b redis https://gitee.com/SHIKEAIXYY/Trss-ComWeChat-Yunzai.git ./redis
+echo '正在下载Qsign'
 git clone --depth 1 https://gitee.com/touchscale/Qsign.git ./Qsign
