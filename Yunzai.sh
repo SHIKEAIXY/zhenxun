0c34494db8a2fbfa4b9081a1e0b38b5e63774820
小雨 <zsfys225011250@qq.com>
2024-03-16 04:17:23 +0000

    update Yunzai.sh.
    Signed-off-by: 小雨 <zsfys225011250@qq.com>

--- Yunzai.sh
+++ Yunzai.sh
@@ -1,3 +1,47 @@
+if ! type git >/dev/null 2>&1; then
+echo '##############################'
+echo 'git未安装！请查看教程手动安装再试'
+echo '##############################'
+exit
+else
+echo 'git 已安装'
+fi
+
+if ! type node >/dev/null 2>&1; then
+echo '##############################'
+echo 'node未安装！请查看教程手动安装再试'
+echo '##############################'
+exit
+else
+echo 'node 已安装'
+fi
+
+if ! type npm >/dev/null 2>&1; then
+echo '##############################'
+echo 'npm未安装！请查看教程手动安装node再试'
+echo '##############################'
+exit
+else
+echo 'npm 已安装'
+fi
+if ! type pnpm >/dev/null 2>&1; then
+echo '##############################'
+echo 'pnpm未安装！'
+echo '##############################'
+echo '正在安装pnpm'
+npm --registry=https://registry.npmmirror.com install pnpm -g
+if ! type pnpm >/dev/null 2>&1; then
+echo '##############################'
+echo 'pnpm安装失败'
+echo '##############################'
+exit
+else
+echo 'pnpm安装成功'
+fi
+else
+echo 'pnpm 已安装'
+fi
+
 echo '正在下载喵崽'
 git clone --depth=1 https://gitee.com/yoimiya-kokomi/Miao-Yunzai.git ./Yunzai/Miao-Yunzai/
 cd Yunzai/Miao-Yunzai
@@ -5,8 +49,6 @@ echo '正在下载喵喵插件'
 git clone --depth=1 https://gitee.com/yoimiya-kokomi/miao-plugin.git ./plugins/miao-plugin/
 echo '正在下载ws插件'
 git clone --depth=1 https://gitee.com/xiaoye12123/ws-plugin.git ./plugins/ws-plugin/
-echo '正在下载pnpm'
-npm --registry=https://registry.npmmirror.com install pnpm -g
 echo '正在安装依赖'
 pnpm i
 echo '正在降级puppeteer'
